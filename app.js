0e0b9bc29f2b3b68703986007fe1000f74bc746d
Shivam Vijaywargi <vjshivam5@gmail.com>
2021-07-21 21:29:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: false,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev"
+  // }
+});
